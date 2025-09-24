94daaf2c9597282e8ce4269e9156497552f257e2
ABDUL MOIZ KHAN <97601652+AMoizKhan@users.noreply.github.com>
2025-09-24 13:22:37 +0500

    Start working

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Header from "@/Component/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+      </div>
+    </div>
+  );
+}
