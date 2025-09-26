09a53b4dd511cc3c2e9a41e5dda9568a18152b92
ABDUL MOIZ KHAN <97601652+AMoizKhan@users.noreply.github.com>
2025-09-26 18:10:10 +0500

    Home page

--- src/app/Home/index.jsx
+++ src/app/Home/index.jsx
@@ -2,6 +2,7 @@ export default function MainPage() {
   return (
     <div className="text-center mt-10">
       <h1 className="text-3xl font-bold">Home Page</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
